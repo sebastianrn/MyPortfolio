54fd128ff7074be0f37229ef824788682179e1ee
Sebastian <sebastian.reissmann@gmail.com>
2019-02-17 15:42:19 +0100

    extracted price class

---
+++ feature/src/main/java/com/myportfolio/sebastian/myportfolio/feature/model/Price.java
@@ -0,0 +1,16 @@
+package com.myportfolio.sebastian.myportfolio.feature.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+    private Pair<Currency, BigDecimal> buyPrice;
+    private Pair<Currency, BigDecimal> sellPrice;
+}
--- feature/src/main/java/com/myportfolio/sebastian/myportfolio/feature/model/PriceListItem.java
+++ feature/src/main/java/com/myportfolio/sebastian/myportfolio/feature/model/PriceListItem.java
@@ -1,11 +1,6 @@
 package com.myportfolio.sebastian.myportfolio.feature.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.tuple.Pair;
-
-import java.math.BigDecimal;
 
 @Data
 public class PriceListItem {
@@ -14,11 +9,4 @@ public class PriceListItem {
     private String weight;
     private Price price;
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class Price {
-        private Pair<Currency, BigDecimal> buyPrice;
-        private Pair<Currency, BigDecimal> sellPrice;
-    }
 }
